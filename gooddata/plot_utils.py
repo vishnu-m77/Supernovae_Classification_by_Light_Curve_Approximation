c2930726958c85f45797d635a1b56deb334c973f
siddhardhap90 <76205050+siddhardhap90@users.noreply.github.com>
2022-11-28 17:50:59 -0500

    added plots.utils. I will push this to main branch tomorrow.

---
+++ gooddata/plot_utils.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as 
+
+
+def plot_light_curves(anobject, passband2name, title=''):
+    anobject = anobject.sort_values('mjd')
+    plt.figure(figsize=(9, 5.5))
+    for passband in range(len(passband2name)):
+        light_curve = get_passband(anobject, passband)
+        plt.scatter(light_curve['mjd'].values, light_curve['flux'].values)
+        plt.plot(light_curve['mjd'].values, light_curve['predFlux'].values, label=passband2name[passband])
+    plt.xlabel('Modified Julian Date')
+    plt.xticks()
+    plt.ylabel('Flux')
+    plt.yticks()
+    plt.legend()
+    plt.title(title)
+
+
+def plotLightCurve(object_, data, predFlux, passband2name, title):
+    '''
+    Parameters:
+    object_: Name of the object for which plot is to be made
+    data: DataFrame containing mjd, Actual Flux, processed passband
+    predFlux: Predicted flux array
+    passband2name: Similar to {0: 'g', 1: 'r'}
+    '''
+    anobject = get_object(data, object_)
+    anobject["predFlux"] = predFlux
+    plot_light_curves(anobject, passband2name, title)
+    plt.show()
+
+
+if __name__ == "__main__":
+    object_ = 'ZTF20adaduxg'
+    passband2name = {0: 'g', 1: 'r'}
+    title = ''
+    # df_obj_filt is original dataframe containing mjd, Actual Flux, processed passband
+    # length of df_obj_filt == len(predFlux)
+    # Tested with if (i+1)%5==0:
+    # to match the length of predicted values with length of other parameters
+    plotLightCurve(object_, df_obj_filt, predFlux, passband2name, title)
